a667738ec26d78d04f5f7471216103bc328b0b26
Benjamin Tallarek / PC <bennitallarek@gmail.com>
2023-01-20 14:49:47 +0100

    design overhaul, using tailwind standard colors now, still temporary design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,6 @@
 module.exports = {
   content: ['./views/**/*.{ejs,html,js}'],
   theme: {
-    colors: {
-      'gun': '#22333B',
-      'dark': '#0F0E0E',
-      'rosso': '#D00000',
-      'bloodred': '#A30000',
-      'text': '#7D8CA3',
-      'blue': '#0645AD',
-      'yellow': '#ffcc00',
-      'green': '#009900',
-      'white': '#FFFFFF',
-      'readonly': 'rgb(221, 148, 87)'
-    },
     extend: {},
   },
   plugins: [],
